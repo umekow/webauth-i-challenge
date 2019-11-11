c6964fabaa178137cd18b58b8cecc9130e54c69e
umekow <umekolashun@gmail.com>
2019-11-11 17:54:16 -0600

    added auth-router, migration, and users-model

---
+++ auth/auth-router.js
@@ -0,0 +1,9 @@
+//imports
+const router = require('express').Router(); 
+const bcrypt = require('bcryptjs'); 
+const Users = require('../users/users-model.js'); 
+
+
+
+//export router
+module.exports = router; 
