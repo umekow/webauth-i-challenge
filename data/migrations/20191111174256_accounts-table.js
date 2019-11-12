b9e001e059823abea886824ba5bd9903e3005c6f
umekow <umekolashun@gmail.com>
2019-11-11 18:06:19 -0600

    changed migration

--- data/migrations/20191111174256_accounts-table.js
+++ data/migrations/20191111174256_accounts-table.js
@@ -1,8 +1,16 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('accounts', accounts => {
+      accounts.increment(); 
+
+      accounts
+        .string('username', 128)
+        .notNullable()
+        .unique(); 
+      accounts.string('password', 128).notNullable(); 
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('accounts'); 
 };
